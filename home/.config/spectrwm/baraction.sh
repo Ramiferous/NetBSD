add9028fc93acf600ac551b7dc4956bf42d4c45b
Ramiferous <david.pfeiffer@protonmail.com>
2020-07-05 23:12:46 +1000

    push updates

--- home/.config/spectrwm/baraction.sh
+++ home/.config/spectrwm/baraction.sh
@@ -4,29 +4,29 @@
 
 ## Echo battery status at regular intevals
 while :; do
-    # Battery Percentage
-    BAT_PERC="$(envstat -s acpibat0:charge | awk 'FNR==3 {print $6}' | tr -d '()')"
-
-    # Battery Charging State
+# Battery Percentage (two decimal places)
+    #BAT_PERC="$(envstat -s acpibat0:charge | awk 'FNR==3 {print $6}' | tr -d '()')"
+# Uncomment the following line to remove the decimal place
+    BAT_PERC="$(envstat -s acpibat0:charge | tail -1 | sed -e 's,.*(\([ ]*[0-9]*\)\..*,\1,g')"
+    
+# Battery Charging State
     BAT_STATE="$(envstat -d acpibat0 | awk 'FNR==10 {print $2}')"
 
 # State detection
 if [ "${BAT_STATE}" = "TRUE" ]; then
-	Bat='charging'
+	STATE='Charging'
 else
-	Bat='discharging'
+	STATE='Discharging'
 fi
 
-    # Master Volume
-    # VOL=$(amixer scontents | awk 'NR==5 {print $4}')
+# Master Volume
+    VOL="$(mixerctl outputs.master | sed -e 's|.*,||g')" # | expr \( $VOL \* 100 \) / 254)"
+    LEVEL="$name $(expr \( $VOL \* 100 \) / 254)"
     # MUTE_STATE=$(amixer scontents | awk 'NR==5 {print $6}')
 
-    # Spotfiy Currently Playing Song and Artist
-    #SPOT_INFO=$(python /home/isaac/.scripts/polybar-spotify/spotify_status.py -f '{artist}: {song}' -t 50)
-
-    # Print Variables
-    echo "$Bat $BAT_PERC |"
-    sleep 30
+# Print Variables
+    echo "$STATE $BAT_PERC% | Vol$LEVEL% |"
+    sleep 1
 done
 
 exit 0
