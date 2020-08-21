02fe54ae32c748b7baa17fe4415257902990da7b
Ramiferous <david.pfeiffer@protonmail.com>
2020-08-21 10:32:40 +1000

    backup

--- home/.config/spectrwm/baraction.sh
+++ home/.config/spectrwm/baraction.sh
@@ -27,7 +27,7 @@ fi
 
 # Weather
     #set -g status-interval 60 
-    WEATHER="$(curl -s wttr.in/-37.731678,144.995142?format='%C+%t\n')"
+#    WEATHER="$(curl -s wttr.in/-37.731665,144.995026?format='%C+%t\n')"
 
 # Packages
     PKGS="$(pkg_info | wc -l | sed -e 's/^[ \t]*//')"
@@ -35,7 +35,39 @@ fi
 # Print Variables
     #echo "$STATE $BAT_PERC% | Vol$LEVEL% | $NETSTAT | $PKGS | $WEATHER" 
     echo "$WEATHER | $PKGS | $STATE $BAT_PERC% | Vol$LEVEL%"
-    sleep 1
+    sleep 10
 done
+
+#update() {
+#    echo \
+#        "$WEATHER |" \
+#	"$PKGS |" \
+#	"$STATE $BAT_PARC% |" \
+#	"Vol$LEVEL%"
+#    return $?
+#}
+
+## For 'reload' to work, we need a PID file.
+#dir="$(dirname "$0")"
+#file="$(basename "$0")"
+#pidfile="$dir/.$file.pid~"
+
+## Reloading works by sending SIGUSR1 to an existing status process.
+#if [ "$1" = "reload" ]; then
+# kill -USR1 "$(cat "$pidfile")"
+# exit $?
+#fi
+
+## Write our own PID, and trap USR1
+## (being sent USR1 will interrupt the 'wait $!' anyways, so we don't need
+## to actually do anything in response)
+#if [ "$1" != "no-pidfile" ]; then
+# echo "$$" > "$pidfile"
+#fi
+#trap true USR1
+
+## 'sleep 1 & wait $!' will sleep for 1 second, but it'll be interrupted
+## by SIGUSR1.
+#while update; do sleep 1 & wait $!; done
 
 exit 0
